50a02e45b8f0c41ce7a541a2739796768571276b
alberto <alberto.crivellaro@epfl.ch>
2016-09-14 18:28:07 +0200

    added launch script!

---
+++ launchSMART_ORBSLAMWithObjTracking.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# TODO add custom catkin_ws path, currently assuming it's in ~
+
+# change these paths if needed!
+ORB_SLAM_ROOT=/home/crivella/programmi/ORB_SLAM2
+
+#--disable-factory needed for getting pid of background terminal
+gnome-terminal --disable-factory -e '/bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && roscore" ' &
+# pid0=$!
+
+sleep 4
+
+gnome-terminal --disable-factory -e '/bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && roscd usb_cam && cd launch/ && roslaunch usb_cam-test.launch " ' &
+
+COMMAND='/bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${ORB_SLAM_INSTALL_ROOT}/Examples/ROS && cd ${ORB_SLAM_INSTALL_ROOT} && rosrun SMART_ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/Monocular/KITTI00-02.yaml" '
+ORB_SLAM_INSTALL_ROOT=${ORB_SLAM_ROOT}  gnome-terminal --disable-factory -e "${COMMAND}" &
+
+ 
+sleep 20
+gnome-terminal --disable-factory -e '/bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && rosrun minimalpnp mainMinimalpnp" ' &
+
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
